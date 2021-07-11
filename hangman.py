bd1eca3c8ef1963358943dfc86c4f902f8c3265b
Vasiliy Belov <belov1@icloud.com>
2021-07-11 21:30:05 +0300

    Complete stage 7/8

--- hangman.py
+++ hangman.py
@@ -5,16 +5,27 @@ print('H A N G M A N')
 words = ['python', 'java', 'kotlin', 'javascript']
 word = random.choice(words)
 result = '-' * len(word)
+wrong_letters = ''
 attempts = 8
 
 while attempts > 0:
     print()
     print(result)
     letter = input('Input a letter:')
-    if letter in result:
-        attempts -= 1
-        print('No improvements')
-    elif letter in word:
+    
+    if len(letter) != 1:
+        print('You should input a single letter')
+        continue
+    
+    if not letter.isalpha() or not letter.islower():
+        print('Please enter a lowercase English letter')
+        continue
+        
+    if letter in result or letter in wrong_letters:
+        print("You've already guessed this letter")
+        continue
+    
+    if letter in word:
         index = 0
         for _ in range(word.count(letter)):
             index = word.find(letter, index)
@@ -22,7 +33,9 @@ while attempts > 0:
         if result == word:
             print('You guessed the word!')
             print('You survived!')
+            break            
     else:
+        wrong_letters += letter
         attempts -= 1
         print("That letter doesn't appear in the word")
 else:
